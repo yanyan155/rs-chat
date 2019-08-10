b895f30857dbcbe2eba94a9b8f1bef9f96e8f2e2
yanyan155 <jm@tut.by>
2019-08-10 23:05:03 +0300

    feat: add files to another deploy

---
+++ rs-chat/build/precache-manifest.7487c1e45997f54804b5d8bc58f5c884.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f89ad2d905af2a8e46cf8cdca11571e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eaa0d5ccb74850c4eebe",
+    "url": "/static/css/main.0e433876.chunk.css"
+  },
+  {
+    "revision": "2de1dc1903ea89584948",
+    "url": "/static/js/2.58508f9c.chunk.js"
+  },
+  {
+    "revision": "eaa0d5ccb74850c4eebe",
+    "url": "/static/js/main.61fc4069.chunk.js"
+  },
+  {
+    "revision": "9501cc90b54b0f8f7a3c",
+    "url": "/static/js/runtime~main.6e0fdbc2.js"
+  }
+]);
