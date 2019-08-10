709e0a79857d5f2b23560f66c9f2f85b1fb5ecfd
yanyan155 <jm@tut.by>
2019-08-10 17:11:53 +0300

    feat: create ws connection

--- rs-chat/src/index.js
+++ rs-chat/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppComponent from './components/AppComponent';
+import wsAPI from './wsAPI';
+
+wsAPI.init();
 
 ReactDOM.render(<AppComponent />, document.getElementById('root'));
