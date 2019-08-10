b3bc76bed82ec2128904745083b256c5f753250e
yanyan155 <jm@tut.by>
2019-08-10 15:05:25 +0300

    feat: create rs-chat scratch project

---
+++ rs-chat/src/components/AppComponent/AppComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AppComponent.css';
+
+function AppComponent() {
+  return (
+    <div className="container">
+      rs-chat
+    </div>
+  );
+}
+
+export default AppComponent;
